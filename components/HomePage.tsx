3f8bc6f7d625a0c20904ce00f88fb205fda64d96
twofoldsam <sam@twofoldai.com>
2025-08-13 22:48:03 +0100

    Remove floating scroll banner (FloatingCTA) from homepage

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Hero } from "./Hero";
 import { Footer } from "./Footer";
-import { FloatingCTA } from "./ui-custom/FloatingCTA";
+// import { FloatingCTA } from "./ui-custom/FloatingCTA";
 import { ContentTypeShowcase } from "./ContentTypeShowcase";
 
 export const HomePage = () => {
@@ -13,8 +13,6 @@ export const HomePage = () => {
         <ContentTypeShowcase />
       </main>
       <Footer />
-      {/* Floating CTA provides persistent conversion opportunity */}
-      <FloatingCTA />
     </>
   );
 }; 
